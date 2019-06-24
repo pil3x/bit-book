d6e5aeb8a5f84e157cf9eb4fc9e4fa992e990fd9
BIT Student <classroom@bgit.rs>
2019-06-24 17:24:07 +0200

    added URL for comments  to API

--- src/app/users/UserItem/UserItem.js
+++ src/app/users/UserItem/UserItem.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const UserItem = (props) => {
     const { user } = props;
+
+
     console.log(props);
     return (
         <>
             <div className="col s4 m4">
                 <div className="card">
                     <div className="card-image">
-                        <img src={user.avatarUrl} alt="alt" />
+                        {<img src={user.avatarUrl} alt="alt" />}
                         <span className="card-title">{user.getFullName()}</span>
                     </div>
                     <div className="card-content">
