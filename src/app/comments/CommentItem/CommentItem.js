479ae1158baf91e710247e9cfdb6067d36309e61
BIT Student <classroom@bgit.rs>
2019-06-25 17:54:21 +0200

    fetchSingleUser function taken from other branch

--- src/app/comments/CommentItem/CommentItem.js
+++ src/app/comments/CommentItem/CommentItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 const CommentItem = (props) => {
 
     const { comment } = props;
-    console.log(comment);
+
     return (
         <li className="collection-item avatar">
             <img src="images/yuna.jpg" alt="" className="circle" />
