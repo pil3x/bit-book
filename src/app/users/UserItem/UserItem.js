ccfb82a88db0ebc343f75960a7f6a0219fd414dc
BIT Student <classroom@bgit.rs>
2019-06-25 12:17:07 +0200

    Added placeholder user image if there is no image

--- src/app/users/UserItem/UserItem.js
+++ src/app/users/UserItem/UserItem.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const UserItem = (props) => {
     const { user } = props;
+    const errorIMG = "https://roundhouse-assets.s3.amazonaws.com/assets/Image/15214-fitandcrop-1200x681.jpg";
     return (
         <>
             <div className="col s4 m4">
                 <div className="card">
                     <div className="card-image">
-                        {<img src={user.avatarUrl} alt="alt" />}
+                        {(!user.avatarUrl) ? <img src={errorIMG} alt="img" /> : <img src={user.avatarUrl} alt="img" />}
                         <span className="card-title">{user.getFullName()}</span>
                     </div>
                     <div className="card-content">
