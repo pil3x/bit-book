d84d6e99c5d78e9cbd9da4863f58bab6c93de7e6
BIT Student <classroom@bgit.rs>
2019-06-26 12:08:17 +0200

    added single user page

--- src/app/users/UserProfile/UserProfile.js
+++ src/app/users/UserProfile/UserProfile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fetchSingleUser } from '../../../services/usersServices.js'
-
+import './user-profile.css'
 class SingleUser extends React.Component {
     constructor(props) {
         super(props);
@@ -29,6 +29,19 @@ class SingleUser extends React.Component {
         }
         return (
             <>
+                <div className="user-profile-main-container">
+                    <div class="user-profile-container">
+                        <div class="user-profile-image-holder">
+                            <img src={user.avatarUrl} alt="img" />
+                            <h4>{user.getFullName()}</h4>
+                        </div>
+                        <p>{user.about}</p>
+                        <div class="user-profile-info-holder">
+                            <p>Posts</p>
+                            <p>Comments</p>
+                        </div>
+                    </div>
+                </div>
 
             </>
 
