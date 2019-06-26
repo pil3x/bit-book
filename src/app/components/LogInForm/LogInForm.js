0bbaacd482377c617ea919ea89f31057fd373a04
BIT Student <classroom@bgit.rs>
2019-06-26 18:07:23 +0200

    created services for fetching logIn token

--- src/app/components/LogInForm/LogInForm.js
+++ src/app/components/LogInForm/LogInForm.js
@@ -12,38 +12,39 @@ class LogInForm extends React.Component {
         }
     }
 
-    componentDidMount() {
-
-    }
-
-    componentDidMount() {
-        this.loadLoginToken()
-    }
-
     onInputChange = (e) => {
-        const name = e.target.value;
         const value = e.target.value;
+        const name = e.target.name;
 
         this.setState({ [name]: value })
     }
 
-    loadLoginToken() {
+    onSubmit = (e) => {
+        e.preventDefault();
+        const { email, password } = this.state;
 
+        //ovde pises i catch i redirectujes ako je sc 200
+
+        console.log(fetchLogInToken(email, password))
     }
 
+
+
     render() {
+        const { onInputChange, onSubmit } = this;
+        const { email, password } = this.state;
 
         return (
             <>
-                <form className="col s12" autoComplete="off">
+                <form className="col s12" autoComplete="off" onSubmit={onSubmit}>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="email" name="email" placeholder="Email" type="email" className="validate" />
+                            <input id="email" name="email" placeholder="Email" type="email" className="validate" value={email} onChange={onInputChange} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input id="password" name="password" placeholder="Password" type="password" className="validate" />
+                            <input id="password" name="password" placeholder="Password" type="password" className="validate" value={password} onChange={onInputChange} />
                         </div>
                     </div>
                     <button className="reg-form-btn btn waves-effect waves-light" type="submit" name="action">Log in
--- src/services/loginServices.js
+++ src/services/loginServices.js
@@ -1,16 +1,28 @@
 const axios = require('axios');
 
 const fetchLogInToken = (email, password) => {
-    return axios.post('https://book-api.hypetech.xyz/v1/auth/login', {
+    return axios({
+        method: 'post',
+        url: 'https://book-api.hypetech.xyz/v1/auth/login',
         headers: {
             "Content-Type": "application/json",
             "x-api-key": "B1tD3V",
         },
-        body: {
-            email: email,
-            password: password
-        }
-    }).then(response => console.log(response))
+        data: { email, password }
+
+    })
+        .then(response => response.data)
+        .catch(error => Promise.reject(error.response.data.message))
 }
 
 export { fetchLogInToken }
+
+
+
+//kad dobijemo response, postavljamo uslov za ispitivanje da li postoje greske, ukoliko postoji greska return
+
+    // u sledecem thenu bi trebalo da dobijemo objekat koji u sebi ima accessToken koji setujemo u localStorage
+    // kad dobijemo te podate u loginPage-u tamo gde smo pozvali funkciju treba da se redirektujemo na feed
+
+    //ako imamo error  imamo catch() na kraju koji vraca neki error message - na kaju pozvane funkcije u loginform komponenti gde mu setujemo stateu tu poruku iz message
+    //taj eror ubacujemo i u state, jer ce nam trebati za rendanje na formi
